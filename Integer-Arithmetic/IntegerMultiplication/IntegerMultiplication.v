33c162ca2059461c214584d558bb0696a4183e7f
Liqimai <liqimai@outlook.com>
2015-04-08 16:21:13 +0800

    Update IntegerMultiplication.v

--- Integer-Arithmetic/IntegerMultiplication/IntegerMultiplication.v
+++ Integer-Arithmetic/IntegerMultiplication/IntegerMultiplication.v
@@ -22,33 +22,15 @@ module IntegerMultiplication(
 	input [31:0] A,
 	input [31:0] B,
 	output [63:0] product,
-	/*
-	output [65:0] accu,
-	output [63:0] res,
-	output [6:0] count,
-	output [32:0] bn2,
-	output [32:0] bn1,
-	output [32:0] bp1,
-	output [32:0] bp2,
-	output [32:0] adden,
-*/
 	input start,
 	input clk
 	);
+	
 	reg [65:0] accumulator;
 	reg [6:0] cnt;
 	wire [32:0] B2n,B1n,B1,B2,addend;
 	wire [63:0] result;
-/*
-assign accu=accumulator,
-		res=result,
-		count =cnt,
-		bn2 = B2n,
-		bn1 = B1n,
-		bp1 = B1,
-		bp2 = B2,
-		adden = addend;
-*/
+	
 assign 	B1n[31:0] = -B,
 		B2n[31:0] = (-B)<<1,
 		B1[31:0] = B,
@@ -76,16 +58,6 @@ assign addend =	{33{~accumulator[2]&~accumulator[1]&~accumulator[0]}}&33'b0|
 						{33{ accumulator[2]& accumulator[1]&~accumulator[0]}}&B1n|
 						{33{ accumulator[2]& accumulator[1]& accumulator[0]}}&33'b0
 						;
-//	case(accumulator[2:0])
-//		3'b000: 0;
-//		3'b001: B1;
-//		3'b010: B1;
-//		3'b011: B2;
-//		3'b100: B2n;
-//		3'b101: B1n;
-//		3'b110: B1n;
-//		3'b111: 0;
-//	endcase
 
 always @(posedge clk) begin
 	if (cnt < 32) begin //accmulating
